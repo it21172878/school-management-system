82f2a82c4eb29271dba81cb3d062158bc49203f8
it21172878 <99156192+it21172878@users.noreply.github.com>
2023-07-28 23:20:53 +0530

    setup

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Hogwart</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="logo.png">
+</head>
+
+<body class="body-home ">
+    <div class="black-fill"><br />
+        <div class="container ">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary" id="homeNav">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="logo.png" width="100">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav me-right mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">LogIn</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <selection class="welcome-text d-flex justify-content-center align-items-center flex-column">
+                <img src="logo.png">
+                <h4>Welcome to Hogwart</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis earum hic atque.</p>
+
+            </selection>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
