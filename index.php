b3e59688e03dac42a768f12df9d3afcdcc8d09fc
it21172878 <99156192+it21172878@users.noreply.github.com>
2023-07-29 11:21:44 +0530

    login.php

--- index.php
+++ index.php
@@ -38,19 +38,19 @@
                         </ul>
                         <ul class="navbar-nav me-right mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">LogIn</a>
+                                <a class="nav-link" href="login.php">LogIn</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
-            <selection class="welcome-text d-flex justify-content-center align-items-center flex-column">
+            <section class="welcome-text d-flex justify-content-center align-items-center flex-column">
                 <img src="logo.png">
                 <h4>Welcome to Hogwart University</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis earum hic atque.</p>
 
-            </selection>
-            <selection id="about" class="d-flex justify-content-center align-items-center flex-column">
+            </section>
+            <section id="about" class="d-flex justify-content-center align-items-center flex-column">
                 <div class="card mb-3 card-1">
                     <div class="row g-0">
                         <div class="col-md-4">
@@ -68,27 +68,37 @@
                     </div>
                 </div>
 
-            </selection>
+            </section>
 
-            <selection id="contact" class="d-flex justify-content-center align-items-center flex-column">
+            <section id="contact" class="d-flex justify-content-center align-items-center flex-column">
                 <form>
+                    <h5>Contact</h5>
                     <div class="mb-30">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
+
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <label class="form-label">Name</label>
+                        <input type="text" class="form-control">
                     </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    <div class="mb-3">
+                        <label class="form-label">Name</label>
+                        <textarea class="form-control" rows="4"></textarea>
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Send</button>
                 </form>
 
-            </selection>
+            </section>
+            <div class="text-center text-light">
+
+                Copyright &copy; <?php
+                    echo date("d.m.Y")." " .date("l").",";
+                    ?>
+                Hogward School. All Rights Reserved.
+
+            </div>
         </div>
     </div>
 
